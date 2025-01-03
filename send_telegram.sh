bde7cca960594036134d8ed16b938aaddfac71f6
Alexiatr <himmatovdilshod06@gmail.com>
2025-01-04 00:21:45 +0500

    Add GitHub Actions workflow for C++ build and Telegram notifications

--- send_telegram.sh
+++ send_telegram.sh
@@ -14,6 +14,7 @@ if [ -z "$MESSAGE" ]; then
 fi
 
 # Xabarni Telegram API orqali yuborish
+
 curl -s -X POST "https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage" \
     -d chat_id=$CHAT_ID \
     -d text="$MESSAGE"
